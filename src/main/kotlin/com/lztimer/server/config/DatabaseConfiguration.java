828931735067a6c299623bb48dea1e5801f32d9b
Krzysztof Kot <krzykot@gmail.com>
2017-07-23 11:47:39 +0200

    auditing user enabled

---
+++ src/main/kotlin/com/lztimer/server/config/DatabaseConfiguration.java
@@ -0,0 +1,31 @@
+package com.lztimer.server.config;
+
+import org.h2.tools.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.sql.SQLException;
+
+@Configuration
+@EnableJpaAuditing
+@EnableTransactionManagement
+public class DatabaseConfiguration {
+    private final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
+
+    /**
+     * Open the TCP port for the H2 database, so it is available remotely.
+     *
+     * @return the H2 database TCP server
+     * @throws SQLException if the server failed to start
+     */
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2TCPServer() throws SQLException {
+        // FIXME: enable only in development profile
+        return Server.createTcpServer("-tcp","-tcpAllowOthers");
+    }
+}
